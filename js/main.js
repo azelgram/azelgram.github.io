92fac4ee686b1ac6d882631e46fb178002cd68f5
Hélio Chun <61918582+azelgram@users.noreply.github.com>
2021-03-31 19:24:35 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,46 +1,132 @@
 
-// Too lazy at this hour of the night for Vanilla
 $(document).ready(function(){
-	
-	$('body').on( 'click', '.playMusicAction', function() {
-		$('.artist-follow').removeClass('showTab');
-		$('.artist-discography').addClass('showTab');
-		$('.actionButton').removeClass('currentButton');
-		$(this).addClass('currentButton');
-	});
-	
-	$('body').on( 'click', '.showFollowAction', function() {
-		$('.artist-discography').removeClass('showTab');
-		$('.artist-follow').addClass('showTab');
-		$('.actionButton').removeClass('currentButton');
-		$(this).addClass('currentButton');
-	});
-	
-	$('body').on( 'click', '.actionButton', function() {
-		$([document.documentElement, document.body]).delay(300).animate({
-			scrollTop: $('.artist-body').offset().top
-		}, 500);
-	});
-	
-	
-	// Music Handlers
-	
-	$('.playSong').click(function(){
-		var $this = $(this);
-		var id = $this.attr('id').replace(/btn/, '');
-		$this.toggleClass('active');
-		if($this.hasClass('active')){
-			$this.text('pause'); 
-			$('audio[id^="sound"]')[id-1].play();        
-		} else {
-			$this.text('play_arrow');
-			$('audio[id^="sound"]')[id-1].pause();
+	var description = '';
+
+	$('#beatPlaylist').ttwMusicPlayer(myPlaylist, {
+		autoPlay:false,
+		duration:true,
+		currencySymbol:'€',
+		description:description,
+		jPlayer:{
+			swfPath:'js/jquery-jplayer' //You need to override the default swf path any time the directory structure changes
 		}
 	});
-	
-	$('#sound1').on('ended', function() {
-		$('.playSong').text('play_arrow');
-		$('.playSong').removeClass('active');
-	});
 
+	$('.togglePlaylist').on('click', function() {
+        $('.tracklist').toggleClass('show-playlist');
+		$('#tracklistDarkPane').addClass('show-darkPane');
+    });
+	
+	$('body,document').on('click', '#fireLisences, .buy', function() {
+        $('.license-container').addClass('show-licenses');
+		$('body').addClass('noScroll');
+		$('#darkPane').addClass('show-darkPane');
+		
+		// Fix when pressed next song
+		$('.track.playing').each(function(i) {
+			var BuyCode = $(this).find('.buy:first').attr('href');
+			var BuyTitle = $(this).find('.title:first').text();
+			$('#paypal-hosted-button').val(BuyCode);
+			$('#buyTitle').html('<span class="material-icons noSelect">music_note</span>'+BuyTitle+'<span class="material-icons noSelect">music_note</span>');
+		});
+    });
+	
+	// Automatically sets Buy Button Code for the first track
+	setTimeout(function(){
+		$('#paypal-hosted-button').val($('.tracks .track:first').find('.buy:first').attr('href'));
+		$('#buyTitle').text($('.tracks .track:first').find('.title:first').text());
+	},1000);
+	
+	// Copies Buy Button values to final Buy Button
+	$('body,document').on('click', '.track', function() {
+		var BuyCode = $(this).find('.buy:first').attr('href');
+		var BuyTitle = $(this).find('.title:first').text();
+		$('#paypal-hosted-button').val(BuyCode);
+		$('#buyTitle').html('<span class="material-icons noSelect">music_note</span>'+BuyTitle+'<span class="material-icons noSelect">music_note</span>');
+    });
+	
+	
+	// Scroll to finalBuy Button
+	$('.licenseBuy').click(function() {
+		$('.License-container').animate({
+			scrollTop: $('#buyScrollPoint').offset().top
+		}, 1000);
+		
+		$('#buyContainer').addClass('highlight-activated');
+		setTimeout(function(){
+			$('#buyContainer').removeClass('highlight-activated');
+		},3000);
+	});
+	
+	// Dark Pane
+	$('#darkPane, #close-buyContainer').on('click', function() {
+		$('body').removeClass('noScroll');
+		$('.site-menu').removeClass('site-menu-open');
+		$('.license-container').removeClass('show-licenses');
+		$('.tracklist').removeClass('show-playlist');
+		$('#darkPane').removeClass('show-darkPane');
+    });
+	
+	// Tracklist Dark Pane
+	$('body,document').on('click', '#tracklistDarkPane, .tracklist-header', function() {
+		$('.tracklist').removeClass('show-playlist');
+		$('#tracklistDarkPane').removeClass('show-darkPane');
+    });
+	
+	
+	// Roll Text
+	var dictionary = "0123456789qwertyuiopasdfghjklzxcvbnm!?></\a`~+*=@#$%".split('');
+
+	var el = document.querySelector('.rollText');
+
+	var ran = function() {
+		return Math.floor(Math.random() * dictionary.length)
+	}
+
+	var ranString = function(amt) {
+		var string = '';
+		for (var i = 0; i < amt; i++) {
+			string += dictionary[ran()];
+		}
+		return string;
+	}
+
+	var rollText = function(str) {
+		var count = str.length;
+		var delay = 50;
+
+		el.innerHTML = '';
+
+		var gen = setInterval(function() {
+			el.setAttribute('data-before', ranString(count));
+			el.setAttribute('data-after', ranString(count));
+			if (delay > 0) {
+				delay--;
+			} else {
+				if (count < str.length) {
+					el.innerHTML += str[str.length - count - 1];
+				}
+				count--;
+				if (count === -1) {
+					clearInterval(gen);
+				}
+			}
+		}, 8);
+	}
+
+	$('.rollText').one( "click", function() {
+		rollText('azelchun@gmail.com');
+		setTimeout(function(){
+			$('.contactMe').removeClass('rollText');
+		},1000);
+	});
+	
+	$('#menu-trigger').on('click', function() {
+		$('body').addClass('noScroll');
+		$('.site-menu').addClass('site-menu-open');
+		$('#darkPane').addClass('show-darkPane');
+    });
+	
+	
 });
+
---
+++ js/myplaylist.js
@@ -0,0 +1,90 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+	{
+        title:'Trap Rock',
+        artist:'Azel Chun',
+        rating:4,
+        buy:'RKTEVJNXTCZGG',
+        price:'25',
+        duration:'150bpm',
+        cover:'mix/1.jpg',
+		mp3:'https://docs.google.com/uc?export=download&id=1UEGhfz80Vb6BGJAF6NfN-6X2wqeRiwls',
+        oga:'https://docs.google.com/uc?export=download&id=1UEGhfz80Vb6BGJAF6NfN-6X2wqeRiwls'
+    },
+	{
+        title:'Trap Rock (Vocals Visualizer)',
+        artist:'Azel Chun',
+        rating:4,
+        buy:'RKTEVJNXTCZGG',
+        price:'25',
+        duration:'150bpm',
+        cover:'mix/1.jpg',
+		mp3:'https://docs.google.com/uc?export=download&id=1IFryCCLkPOzq7yCeoiMAgoLQ6fvZBLW7',
+        oga:'https://docs.google.com/uc?export=download&id=1IFryCCLkPOzq7yCeoiMAgoLQ6fvZBLW7'
+    },
+	{
+        title:'Martian Cashier (Megan Thee Stallion Type Beat)',
+        artist:'Azelgram',
+        rating:3,
+        buy:'#',
+        price:'25',
+        duration:'140bpm',
+        cover:'mix/2.jpg',
+		mp3:'https://docs.google.com/uc?export=download&id=17xC2Fj2p4d5EN4blcY8qm8iXiMmq-J-v',
+        oga:'https://docs.google.com/uc?export=download&id=17xC2Fj2p4d5EN4blcY8qm8iXiMmq-J-v'
+    },
+	{
+        title:'Martian Cashier (Vocals Visualizer)',
+        artist:'Azelgram',
+        rating:3,
+        buy:'#',
+        price:'25',
+        duration:'140bpm',
+        cover:'mix/2.jpg',
+		mp3:'https://docs.google.com/uc?export=download&id=1JNGUDt5-MEKldzp-zkdWjXMaGZ-yKy6H',
+        oga:'https://docs.google.com/uc?export=download&id=1JNGUDt5-MEKldzp-zkdWjXMaGZ-yKy6H'
+    },
+    {
+        title:'Gucci Bands (Gucci Mane Type Beat)',
+        artist:'Azelgram',
+        rating:4,
+        buy:'#',
+        price:'25',
+        duration:'122bpm',
+        cover:'mix/3.jpg',
+		mp3:'https://docs.google.com/uc?export=download&id=1m-uOCa5-uz6aT0IeSlkc5eS0S5QxSTmi',
+        oga:'https://docs.google.com/uc?export=download&id=1m-uOCa5-uz6aT0IeSlkc5eS0S5QxSTmi'
+    },
+    {
+        title:'Heat the Bass',
+        artist:'Azelgram',
+        rating:2,
+        buy:'#',
+        price:'25',
+        duration:'185bpm',
+        cover:'mix/4.jpg',
+		mp3:'https://docs.google.com/uc?export=download&id=15smUh6x_3MtHyOCVQ5NnP3iTxtARyeHV',
+        oga:'https://docs.google.com/uc?export=download&id=15smUh6x_3MtHyOCVQ5NnP3iTxtARyeHV'
+    },
+    {
+        title:'Purple Vibes (Wale, Wiz Khalifa Type Beat)',
+        artist:'Azelgram',
+        rating:5,
+        buy:'#',
+        price:'25',
+        duration:'90bpm',
+        cover:'mix/5.jpg',
+		mp3:'https://docs.google.com/uc?export=download&id=1Gkpmkyp5KN21ZxLW97ovaRL44csfWobF',
+        oga:'https://docs.google.com/uc?export=download&id=1Gkpmkyp5KN21ZxLW97ovaRL44csfWobF',
+    },
+    
+
+];
